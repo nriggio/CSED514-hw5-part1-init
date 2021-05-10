151c7d3ce5bfcc982db3bb42773b29ee8d53f13f
Nicole Riggio <77315868+nriggio@users.noreply.github.com>
2021-05-10 17:15:29 -0400

    test adddoses queries

--- testing.sql
+++ testing.sql
@@ -14,3 +14,11 @@ Drop Table Vaccines;
 Drop Table VaccineAppointments;
 
 EXEC sp_fkeys 'CareGiverSchedule'
+
+-- TEST Vaccine addition queries
+INSERT INTO Vaccines (VaccineName, DosesRequired, MaxSpacing, MinSpacing, MaxStorageTemp) 
+VALUES ('Johnson & Johnson', '1', '0', '0', '0');
+
+UPDATE Vaccines SET TotalDoses = TotalDoses + 100 WHERE VaccineName = 'Johnson & Johnson';
+
+SELECT * FROM Vaccines WHERE VaccineName = 'Johnson & Johnson'
