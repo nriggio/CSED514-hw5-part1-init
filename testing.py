8a8d20638c022a7a37dcad888d618b529562bbd3
Nicole Riggio <77315868+nriggio@users.noreply.github.com>
2021-05-10 19:39:51 -0400

    junk python testing

--- testing.py
+++ testing.py
@@ -1,5 +1,27 @@
-#test
+# imports
+import unittest
+import os
+
+from sql_connection_manager import SqlConnectionManager
+from vaccine_caregiver import VaccineCaregiver
+from enums import *
+from utils import *
+from covid19_vaccine import COVID19Vaccine as covid
+
+
+# test string concatenation
 VaccineName = 'Pfizer'
+DosesToAdd = 100
 _sqlCHECK = ("SELECT * FROM Vaccines WHERE '")
 _sqlCHECK += str(VaccineName) + "'"
 print(_sqlCHECK)
+
+_sqltext = ("UPDATE Vaccines SET TotalDoses = (TotalDoses + ")
+_sqltext += str(DosesToAdd) + ") WHERE " + "'" + str(VaccineName) + "'"
+print(_sqltext)
+
+VaccineName = 'Pfizer'
+DosesToReserve = 2
+_sqltext2 = ("UPDATE Vaccines SET DosesReserved = (DosesReserved + ")
+_sqltext2 += str(DosesToReserve) + ") WHERE VaccineName = " + "'" + str(VaccineName) + "'"
+print(_sqltext2)
--- testing.sql
+++ testing.sql
@@ -15,10 +15,14 @@ Drop Table VaccineAppointments;
 
 EXEC sp_fkeys 'CareGiverSchedule'
 
--- TEST Vaccine addition queries
-INSERT INTO Vaccines (VaccineName, DosesRequired, MaxSpacing, MinSpacing, MaxStorageTemp) 
-VALUES ('Johnson & Johnson', '1', '0', '0', '0');
+-- TEST Vaccine addition + reservation queries
+INSERT INTO Vaccines (VaccineName, DosesRequired, MaxSpacing, MinSpacing, MaxStorageTemp) VALUES ('Johnson & Johnson', '1', '0', '0', '0');
+INSERT INTO Vaccines (VaccineName, DosesRequired, MaxSpacing, MinSpacing, MaxStorageTemp) VALUES ('Moderna', '2', '0', '0', '0');
 
 UPDATE Vaccines SET TotalDoses = TotalDoses + 100 WHERE VaccineName = 'Johnson & Johnson';
 
-SELECT * FROM Vaccines WHERE VaccineName = 'Johnson & Johnson'
+SELECT * FROM Vaccines
+UPDATE Vaccines SET DosesReserved = DosesReserved + DosesRequired WHERE VaccineName = 'Johnson & Johnson';
+UPDATE Vaccines SET DosesReserved = DosesReserved + DosesRequired WHERE VaccineName = 'Moderna';
+SELECT * FROM Vaccines
+
